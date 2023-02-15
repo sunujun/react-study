696c46c62d15685b89ec2419edeb1f8cf4bd71dd
seongjun.oh <tiger8708@gmail.com>
2023-02-15 13:31:52 +0900

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,117 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
-const Header = ({
-  title,
-  onChangeMode,
-}: {
-  title: string;
-  onChangeMode: () => void;
-}) => {
-  return (
-    <header>
-      <h1>
-        <a
-          href="/"
-          onClick={(event) => {
-            event.preventDefault();
-            onChangeMode();
-          }}
-        >
-          {title}
-        </a>
-      </h1>
-    </header>
-  );
+const Header = ({ title, onChangeMode }: { title: string; onChangeMode: () => void }) => {
+    return (
+        <header>
+            <h1>
+                <a
+                    href="/"
+                    onClick={event => {
+                        event.preventDefault();
+                        onChangeMode();
+                    }}>
+                    {title}
+                </a>
+            </h1>
+        </header>
+    );
 };
 
 const Nav = ({
-  topics,
-  onChangeMode,
+    topics,
+    onChangeMode,
 }: {
-  topics: {
-    id: number;
-    title: string;
-    body: string;
-  }[];
-  onChangeMode: (id: number) => void;
+    topics: {
+        id: number;
+        title: string;
+        body: string;
+    }[];
+    onChangeMode: (id: number) => void;
 }) => {
-  const lis = [];
-  for (let i = 0; i < topics.length; i++) {
-    let t = topics[i];
-    lis.push(
-      <li key={t.id}>
-        <a
-          id={t.id.toString()}
-          href={"/read/" + t.id}
-          onClick={(event) => {
-            event.preventDefault();
-            onChangeMode(Number((event.target as HTMLAnchorElement).id));
-          }}
-        >
-          {t.title}
-        </a>
-      </li>
-    );
-  }
+    const lis = [];
+    for (let i = 0; i < topics.length; i++) {
+        let t = topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a
+                    id={t.id.toString()}
+                    href={'/read/' + t.id}
+                    onClick={event => {
+                        event.preventDefault();
+                        onChangeMode(parseInt((event.target as HTMLAnchorElement).id));
+                    }}>
+                    {t.title}
+                </a>
+            </li>,
+        );
+    }
 
-  return (
-    <nav>
-      <ol>{lis}</ol>
-    </nav>
-  );
+    return (
+        <nav>
+            <ol>{lis}</ol>
+        </nav>
+    );
 };
 
 const Article = ({ title, body }: { title: string; body: string }) => {
-  return (
-    <article>
-      <h2>{title}</h2>
-      {body}
-    </article>
-  );
+    return (
+        <article>
+            <h2>{title}</h2>
+            {body}
+        </article>
+    );
 };
 
 const App = () => {
-  const topics = [
-    { id: 1, title: "html", body: "html is ..." },
-    { id: 2, title: "css", body: "css is ..." },
-    { id: 3, title: "javascript", body: "javascript is ..." },
-  ];
+    const [mode, setMode] = useState('WELCOME');
+    const [id, setId] = useState<number | null>(null);
+    const topics = [
+        { id: 1, title: 'html', body: 'html is ...' },
+        { id: 2, title: 'css', body: 'css is ...' },
+        { id: 3, title: 'javascript', body: 'javascript is ...' },
+    ];
+    let content = null;
+    if (mode === 'WELCOME') {
+        content = <Article title="Welcome" body="Hello, WEB" />;
+    } else if (mode === 'READ') {
+        let title = '';
+        let body = '';
+        // for (let i = 0; i < topics.length; i++) {
+        //     if (topics[i].id === id) {
+        //         title = topics[i].title;
+        //         body = topics[i].body;
+        //         break;
+        //     }
+        // }
+        for (const topic of topics) {
+            if (topic.id === id) {
+                title = topic.title;
+                body = topic.body;
+                break;
+            }
+        }
+        content = <Article title={title} body={body} />;
+    }
 
-  return (
-    <div>
-      <Header
-        title="WEB"
-        onChangeMode={() => {
-          alert("Header");
-        }}
-      />
-      <Nav
-        topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
-        }}
-      />
-      <Article title="Welcome" body="Hello, WEB" />
-    </div>
-  );
+    return (
+        <div>
+            <Header
+                title="WEB"
+                onChangeMode={() => {
+                    setMode('WELCOME');
+                }}
+            />
+            <Nav
+                topics={topics}
+                onChangeMode={_id => {
+                    setMode('READ');
+                    setId(_id);
+                }}
+            />
+            {content}
+        </div>
+    );
 };
 
 export default App;
