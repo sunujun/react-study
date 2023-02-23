1a94caf22b1669484b362fbb56181183279031ea
seongjun.oh <tiger8708@gmail.com>
2023-02-23 20:02:23 +0900

    사용할 필요없는 함수 제거

--- src/toDoList/hooks/useToDoList.ts
+++ src/toDoList/hooks/useToDoList.ts
@@ -12,9 +12,6 @@ export const useToDoList = (selectedDate: dayjs.Dayjs) => {
     const [toDoList, setToDoList] = useState<ToDoList[]>([]);
     const [input, setInput] = useState('');
 
-    const saveToDoList = (newToDoList: ToDoList[]) => {
-        setToDoList(newToDoList);
-    };
     const addToDo = () => {
         const lastId = toDoList.length === 0 ? 0 : toDoList[toDoList.length - 1].id;
         const newToDoList: ToDoList[] = [
@@ -26,11 +23,11 @@ export const useToDoList = (selectedDate: dayjs.Dayjs) => {
                 isSuccess: false,
             },
         ];
-        saveToDoList(newToDoList);
+        setToDoList(newToDoList);
     };
     const removeToDo = (toDoId: number) => {
         const newToDoList = toDoList.filter(toDo => toDo.id !== toDoId);
-        saveToDoList(newToDoList);
+        setToDoList(newToDoList);
     };
     const toggleToDo = (toDoId: number) => {
         const newToDoList = toDoList.map(toDo => {
@@ -42,7 +39,7 @@ export const useToDoList = (selectedDate: dayjs.Dayjs) => {
                 isSuccess: !toDo.isSuccess,
             };
         });
-        saveToDoList(newToDoList);
+        setToDoList(newToDoList);
     };
     const resetInput = () => {
         setInput('');
