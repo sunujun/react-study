573ec9e9bc0d0c35eeb617a40b48b6ae7966592c
seongjun.oh <tiger8708@gmail.com>
2023-03-07 21:32:30 +0900

    App.tsx 파일에서 Provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import './App.css';
-import { Provider } from 'react-redux';
 import { TodoList } from './todoList/TodoList';
-import store from './todoList/store';
 
 const App = () => {
-    return (
-        <Provider store={store}>
-            <TodoList />
-        </Provider>
-    );
+    return <TodoList />;
 };
 
 export default App;
